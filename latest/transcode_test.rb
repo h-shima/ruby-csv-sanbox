dc1f01aea1cebb57ae50adb7d4558c987a547503
Hiroki Oshima <geet1876@gmail.com>
2020-08-23 18:25:30 +0900

    Add latest(3.1.7) version tests

---
+++ latest/transcode_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'csv'
+
+class TestTranscode < Minitest::Test
+  # このテストでは出力される文字コードがutf-8になっている
+  def test_transcode_with_multibyte_char
+    CSV.generate(encoding: Encoding::Windows_31J) do |csv|
+      csv << ["あいう", "え\u2783お"]
+      csv << ["かきく", "け\u2783こ\uF941\uF90A"]
+      assert_equal csv.encoding, csv.string.encoding
+    end
+  end
+
+  def test_transcode_only_ascii
+    CSV.generate(encoding: Encoding::Windows_31J) do |csv|
+      csv << ["a", "b", "c"]
+      csv << ["d", "e"]
+      assert_equal csv.encoding, csv.string.encoding
+    end
+  end
+end
